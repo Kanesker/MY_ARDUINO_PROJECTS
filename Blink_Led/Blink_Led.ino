1085e79dba5f6c58bf843327384d5158d5c0e63c
Kanesker.P <107111175+Kanesker@users.noreply.github.com>
2026-03-14 21:13:16 +0530

    Add files via upload

---
+++ Blink_Led/Blink_Led.ino
@@ -0,0 +1,31 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(12,OUTPUT);
+  pinMode(7,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12,HIGH);
+  delay(200);
+  digitalWrite(12,LOW);
+  delay(500);
+  digitalWrite(12,HIGH);
+  delay(500);
+  digitalWrite(12,LOW);
+  delay(200);
+  digitalWrite(7,HIGH);
+  delay(1500);
+  digitalWrite(7,LOW);
+  delay(1000);
+  digitalWrite(12,HIGH);
+  delay(500);
+  digitalWrite(12,LOW);
+  delay(500);
+  digitalWrite(7,HIGH);
+  delay(1500);
+  digitalWrite(7,LOW);
+  delay(1000);
+}
+  
+ 
